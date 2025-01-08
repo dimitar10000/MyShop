a866d57389ff095e903ff6f7a6ac6eb7342cd96a
dimitar10000 <dimitar_eco@abv.bg>
2025-01-08 14:03:07 +0200

    Added get and update user server actions

---
+++ src/app/actions/user.tsx
@@ -0,0 +1,46 @@
+'use server'
+import type { User } from '@/app/lib/definitions';
+import { PrismaClient } from '@prisma/client'
+
+export async function getUser(email: string) {
+    const prisma = new PrismaClient();
+    let user: User | null;
+
+    try {
+        user = await prisma.users.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+    } catch (e: any) {
+        console.error(e);
+        return null;
+    } finally {
+        await prisma.$disconnect();
+    }
+
+    return user;
+}
+
+export async function updateUser(user: User) {
+    const prisma = new PrismaClient();
+
+    try {
+        await prisma.users.updateMany({
+            where: {
+                email: user.email
+            },
+            data: {
+                given_name: user.givenName,
+                family_name: user.familyName,
+                phone: user.phone
+            }
+        })
+    } catch (e: any) {
+        console.error(e);
+        return null;
+    } finally {
+        await prisma.$disconnect();
+    }
+}
