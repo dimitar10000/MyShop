fb194c9ccac02ae1d7c41947d77bd381788967cf
dimitar10000 <dimitar_eco@abv.bg>
2025-01-22 11:48:45 +0200

    Fixed shopping box insurance visibility

--- src/app/components/header/shopping-cart/shopping-cart.tsx
+++ src/app/components/header/shopping-cart/shopping-cart.tsx
@@ -3,6 +3,7 @@ import ShoppingCartOutlined from "@mui/icons-material/ShoppingCartOutlined";
 import Link from "next/link";
 import {ShoppingCartItemType,Nullable} from '@/app/lib/definitions';
 import CartMenu from '@/app/components/menus/cart-menu';
+import Badge from '@mui/material/Badge';
 import { useEffect, useState } from 'react'
 
 export default function ShoppingCart({cartItems} : {cartItems: Nullable<ShoppingCartItemType[]>}) {
@@ -14,12 +15,16 @@ export default function ShoppingCart({cartItems} : {cartItems: Nullable<Shopping
         }
     },[cartItems]);
 
+    console.log('num cart items', numCartItems);
+
     return (
         numCartItems === 0
         ? <div className="flex flex-row items-center hover:brightness-90">
             <Link href={'/shopping-cart'}>
                 <div className='flex flex-row items-center'>
+                    <Badge badgeContent={"0"} color="success">
                         <ShoppingCartOutlined fontSize="large" />
+                    </Badge>
                     <span className='text-center'> Shopping Cart </span>
                 </div>
             </Link>
